33799e52bb8c8b2df3bf426c5de8f57d7d3a061a
Taojie Wang <1138535467@qq.com>
2026-02-13 06:31:27 +0000

    implement a simple SGD optimizer and test with multiple learning rates

--- tests/training.py
+++ tests/training.py
@@ -1,7 +1,10 @@
 import torch
 from torch import Tensor
 from einops import rearrange
+from collections.abc import Callable, Iterable
 from jaxtyping import Float, Int
+from typing import Optional
+import math
 
 
 
@@ -34,3 +37,42 @@ class CrossEntropy(torch.nn.Module):
         # calculate loss
         loss = log_sum_exp - target_logits
         return torch.mean(loss)
+    
+    
+class SGD(torch.optim.Optimizer):
+    def __init__(self,params,lr=1e-3):
+        if lr < 0:
+            raise ValueError(f"Invalid learning rate: {lr}")
+        defaults = {"lr":lr}
+        super().__init__(params,defaults)
+        
+    def step(self, closure: Optional[Callable] = None):
+        loss = None if closure is None else closure()
+        for group in self.param_groups:
+            lr = group['lr']  # get the learning rate
+            for p in group['params']:
+                if p.grad is None:
+                    continue
+                
+                state = self.state[p]
+                t = state.get("t", 0)
+                grad = p.grad.data 
+                p.data -= lr / math.sqrt(t+1) * grad
+                state['t'] = t+1
+                
+        return loss
+                
+if __name__ == "__main__":
+    for lr in [1, 10, 100]:
+        print(f"\n--- Testing lr={lr} ---")
+        weight = torch.nn.Parameter(5 * torch.randn((10, 10)))
+        opt = SGD([weight], lr=lr)
+        print(f"\nThe current lr: {lr}")
+        
+        for t in range(10):
+            opt.zero_grad()
+            loss = (weight**2).mean()
+            print(f"Iteration {t}, Loss: {loss.item():.6f}")
+            loss.backward()
+            opt.step()
+    
